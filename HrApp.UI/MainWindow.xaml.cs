c09f4a8a6b68d3d6bc7ddbeced4789aae98fe691
Wim Bervoets <wim.bervoets@pxl.be>
2025-02-06 22:26:51 +0100

    Updated template

--- HrApp.UI/MainWindow.xaml.cs
+++ HrApp.UI/MainWindow.xaml.cs
@@ -16,14 +16,9 @@ namespace HrApp.UI
     /// </summary>
     public partial class MainWindow : Window
     {
-        private List<string> _employees;
-
         public MainWindow()
         {
             InitializeComponent();
-
-            _employees = new List<string>();
-            employeesListBox.DataContext = _employees;
         }
 
         private void OnAddEmployee_Clicked(object sender, RoutedEventArgs e)
@@ -32,7 +27,7 @@ namespace HrApp.UI
             {
                 //TODO: Refactor code -> create Employee instance and add to listbox
                 string fullName = $"{firstNameTextBox.Text} {lastNameTextBox.Text} ({birthDatePicker.SelectedDate}) - {salaryTextBox.Text:c}";
-                _employees.Add(fullName);
+                employeesListBox.Items.Add(fullName);
             }
             catch (Exception ex)
             {
@@ -40,49 +35,11 @@ namespace HrApp.UI
             }
         }
 
-        private void SortEmployees(string property, bool reversed)
-        {
-            //TODO: Implement sorting
-            throw new NotImplementedException();
-        }
-
         private void OnSelectedEmployeeChanged(object sender, SelectionChangedEventArgs e)
         {
             //TODO: Show details of selected employee
         }
 
-        #region Sort Button Events
-        private void OnSortFirstNameDown_Clicked(object sender, RoutedEventArgs e)
-        {
-            SortEmployees("firstName", false);
-        }
-
-        private void OnSortLastNameDown_Clicked(object sender, RoutedEventArgs e)
-        {
-            SortEmployees("lastName", false);
-        }
-
-        private void OnSortAgeDown_Clicked(object sender, RoutedEventArgs e)
-        {
-            SortEmployees("birthDate", false);
-        }
-
-        private void OnSortFirstNameUp_Clicked(object sender, RoutedEventArgs e)
-        {
-            SortEmployees("firstName", true);
-        }
-
-        private void OnSortLastNameUp_Clicked(object sender, RoutedEventArgs e)
-        {
-            SortEmployees("lastName", true);
-        }
-
-        private void OnSortAgeUp_Clicked(object sender, RoutedEventArgs e)
-        {
-            SortEmployees("birthDate", true);
-        }
-
-        #endregion
 
     }
 }
