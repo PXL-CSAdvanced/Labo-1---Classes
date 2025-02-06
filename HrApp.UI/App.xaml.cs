72b63102c34e30145ff9721fe25651b8150d8c11
Wim Bervoets <wim.bervoets@pxl.be>
2025-02-06 21:13:11 +0100

    Add project files.

---
+++ HrApp.UI/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HrApp.UI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ HrApp.UI/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HrApp.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<string> _employees;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _employees = new List<string>();
+            employeesListBox.DataContext = _employees;
+        }
+
+        private void OnAddEmployee_Clicked(object sender, RoutedEventArgs e)
+        {
+            string fullName = $"{firstNameTextBox.Text} {lastNameTextBox.Text} ({birthDatePicker.SelectedDate})";
+
+            _employees.Add(fullName);
+        }
+
+        private void SortEmployees(string property, bool reversed)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region Sort Button Events
+        private void OnSortFirstNameDown_Clicked(object sender, RoutedEventArgs e)
+        {
+            SortEmployees("firstName", false);
+        }
+
+        private void OnSortLastNameDown_Clicked(object sender, RoutedEventArgs e)
+        {
+            SortEmployees("lastName", false);
+        }
+
+        private void OnSortAgeDown_Clicked(object sender, RoutedEventArgs e)
+        {
+            SortEmployees("birthDate", false);
+        }
+
+        private void OnSortFirstNameUp_Clicked(object sender, RoutedEventArgs e)
+        {
+            SortEmployees("firstName", true);
+        }
+
+        private void OnSortLastNameUp_Clicked(object sender, RoutedEventArgs e)
+        {
+            SortEmployees("lastName", true);
+        }
+
+        private void OnSortAgeUp_Clicked(object sender, RoutedEventArgs e)
+        {
+            SortEmployees("birthDate", true);
+        }
+        #endregion
+
+
+    }
+}
